8d7d7983801745ae25ffe00a532a20932613d0a9
Evgeniy Adylov <openrestrictions@gmail.com>
2024-08-27 02:16:18 +0300

    changed ignore files and added asciinema

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -22,8 +22,7 @@ public class Engine {
                 countRightAnswer++;
                 System.out.println("Correct!");
             } else {
-                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '"
-                        + correctAnswer + "'.\nLet's try again!");
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.\nLet's try again!");
                 return;
             }
         }
